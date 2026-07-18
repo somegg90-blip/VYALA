dfd9ecbd5aa6678f199b032dc2b64d18e2a0bbec
somegg90-blip <somegg90@gmail.com>
2026-07-18 14:20:22 +0000

    Re-trigger with final artifact name fix 1.2

--- api/test_auth.py
+++ api/test_auth.py
@@ -2,3 +2,4 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
 # re-trigger after run-id fix
 # final-fix
+# final-fix 1.2
